dbcbd8febcba380b2b374bf95dee076678ab2fb0
bluace92 <eden920.dev@gmail.com>
2020-02-15 10:53:46 +0900

    Automated Commision

--- run.py
+++ run.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
+import os
 from stock import upload
 from service import select_model, download_stock, run, git_update
 
 if __name__ == "__main__":
-    model = select_model()
-    print("Model = [", model, "]\n")
+    while True:
+        model = select_model()
+        print("Model = [", model, "]\n")
 
-    path, id = download_stock()
-    st = upload(path, True)
+        path, id = download_stock()
+        st = upload(path, True)
 
-    raw_prediction = run(st, model)
-    print(raw_prediction, "\n")
-    git_update()
+        raw_prediction = run(st, model)
+        print(raw_prediction, "\n")
+        git_update()
+        os.system('clear')
--- train.py
+++ train.py
@@ -18,34 +18,36 @@ time.sleep(2)
 os.system('clear')
 
 if __name__ == "__main__":
-     # use service module to download stock from YAHOO
-     # downloaded stock will be saved as "Database/stock.csv"
-    csv, id = download_stock()
-    print("\nDownloaded ", id, " historical stock data [PATH=Database/stock.csv]")
+    while True:
+        # use service module to download stock from YAHOO
+        # downloaded stock will be saved as "Database/stock.csv"
+        csv, id = download_stock()
+        print("\nDownloaded ", id, " historical stock data [PATH=Database/stock.csv]")
 
-    stock = upload(csv, True)
-    dataset = partition_time_series(stock, QUARTER) # each time series will be a quarter-long (90 datapoints)
+        stock = upload(csv, True)
+        dataset = partition_time_series(stock, QUARTER) # each time series will be a quarter-long (90 datapoints)
 
-    # compute trend line of each time series
-    loop = tqdm.tqdm(total = len(dataset), position = 0, leave = False)
-    for timeseries in dataset:
-        loop.set_description('Time series trend line analysis/sampling... ' .format(len(dataset)))
-        matrix, prediction = rolling_mean_trend(timeseries, MONTH)
-        matrix = sampling(matrix, 0, 2, STANDARD_SAMPLING_RANGE)
-        timeseries.set_sampled_matrix(matrix)
-        loop.update(1)
-    print("\nCompleted trend line analysis")
-    loop.close()
+        # compute trend line of each time series
+        loop = tqdm.tqdm(total = len(dataset), position = 0, leave = False)
+        for timeseries in dataset:
+            loop.set_description('Time series trend line analysis/sampling... ' .format(len(dataset)))
+            matrix, prediction = rolling_mean_trend(timeseries, MONTH)
+            matrix = sampling(matrix, 0, 2, STANDARD_SAMPLING_RANGE)
+            timeseries.set_sampled_matrix(matrix)
+            loop.update(1)
+        print("\nCompleted trend line analysis")
+        loop.close()
 
-    # check if there is an existing model for the corresponding stock ID
-    # if model exists, load existing model and train on it (model.Model: update())
-    # if not, create new model (model.KerasTrainer)
-    try:
-        f = open("Models/" + id + "_model.h5", "r")
-        model = model.Model(id)
-        model.update(dataset, True, 10, 32)
-        f.close()
-    except IOError:
-        model = model.KerasTrainer(dataset, id.lower())
-        model.train(True, 10, 32)
-    git_update()
+        # check if there is an existing model for the corresponding stock ID
+        # if model exists, load existing model and train on it (model.Model: update())
+        # if not, create new model (model.KerasTrainer)
+        try:
+            f = open("Models/" + id + "_model.h5", "r")
+            model = model.Model(id)
+            model.update(dataset, True, 10, 32)
+            f.close()
+        except IOError:
+            model = model.KerasTrainer(dataset, id.lower())
+            model.train(True, 10, 32)
+        git_update()
+        os.system('clear')
