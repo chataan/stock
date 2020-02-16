6d84bf722bff2940cdb3a0968b7d679cc5c2ac13
junyoung-sim <eden920.dev@gmail.com>
2020-02-16 13:16:51 +0900

    Automated Commision

---
+++ long-term.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from stock import upload
+from service import select_model, download_stock, git_update
+from financial import fetch_last_time_series
+
+if __name__ == "__main__":
+    model = select_model()
+    print("Model = [", model, "]\n")
+
+    path, id = download_stock()
+    st = upload(path, True)
+    timeseries = fetch_last_series(st, 90)
+
+    print(timeseries)
+    git_update()
