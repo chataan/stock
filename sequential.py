b7e990bc08644e29b9dd25ad980e33d4373294c7
junyoung-sim <eden920.dev@gmail.com>
2020-02-20 19:42:42 +0900

    Automated Commision

--- sequential.py
+++ sequential.py
@@ -6,17 +6,16 @@ from service import graph, select_model, download_stock, sequential_prediction, 
 from financial import fetch_last_time_series
 
 if __name__ == "__main__":
-    while True:
-        model = select_model()
-        print("Model = [", model, "]\n")
+    model = select_model()
+    print("Model = [", model, "]\n")
 
-        path, id = download_stock()
-        st = upload(path, True)
+    path, id = download_stock()
+    st = upload(path, True)
     
-        prediction_matrix = sequential_prediction(st, 10, model)
-        print(prediction_matrix)
-        graph_title = model + "_sequential_prediction_demo"
-        graph(prediction_matrix, 'red', graph_title, False)
+    prediction_matrix = sequential_prediction(st, 10, model)
+    print(prediction_matrix)
+    graph_title = model + "_sequential_prediction_demo"
+    graph(prediction_matrix, 'red', graph_title, False)
     
-        git_update()
-        os.system('clear')
+    git_update()
+    os.system('clear')
