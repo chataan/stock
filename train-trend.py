d4ae0b2eb6d3cfd38240865d11ccc625ada18df4
junyoung-sim <eden920.dev@gmail.com>
2020-02-22 22:08:00 +0900

    Automated Commision

--- train-trend.py
+++ train-trend.py
@@ -54,5 +54,5 @@ if __name__ == "__main__":
     except IOError:
         m = KerasTrainer()
         m.feed(id.lower(), training_input, training_output, validation_input, validation_output)
-        m.train("Trend Models", True, 10, 32)
+        m.train("Trend-Models", True, 10, 32)
     git_update()
