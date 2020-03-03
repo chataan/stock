34dd0af206a61f9d3bfe9f00e1b26f7d01261920
junyoung-sim <eden920.dev@gmail.com>
2020-03-03 12:21:03 +0900

    Automated Commision

--- momentum.py
+++ momentum.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from service import download_stock
+from service import download_stock, git_update
 from stock import upload
 from invest import momentum_investing
 
@@ -8,4 +8,5 @@ from invest import momentum_investing
 if __name__ == "__main__":
     csv, id = download_stock()
     stock = upload(csv, 5, True)
-    momentum_investing(stock, 10000000, 12, 5)
+    momentum_investing(stock, 100000000, 12, 5)
+    git_update()
