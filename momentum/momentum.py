7e78d1bdb1f97473586193ea3094740992ba8c57
junyoung-sim <eden920.dev@gmail.com>
2020-03-06 21:41:28 +0900

    Automated Commision

--- momentum/momentum.py
+++ momentum/momentum.py
@@ -14,16 +14,17 @@ def momentum_investing(stock, principal, evaluate_range, evaluate_period):
     for _range in range(evaluate_range, len(stock) - evaluate_range, evaluate_period):
         momentum = 0
         total_asset = int(cash + (stock[_range] * shares))
-        change = ((total_asset - previous_asset) / total_asset) * 100
+        change = ((total_asset - principal) / principal) * 100
         for i in range(_range, _range - evaluate_range, -1):
             if stock[_range] > stock[i]:
                 momentum += 1
         if momentum != 0:
             stock_asset = int(total_asset * (momentum / evaluate_range))
             shares = int(stock_asset / stock[_range])
-            cash = total_asset - stock_asset
+            cash = total_asset - shares * stock[_range]
         previous_asset = total_asset
         print("Total = ", total_asset, " [ Stock = ", stock_asset, ", Cash = ", cash, "] --> Shares: ", shares, ", < CHANGE=", change, ">")
+    print("Final Profit = ", (total_asset - principal) * 100 / principal)
     print(stock[0])
 
 if __name__ == "__main__":
