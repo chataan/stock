8e3ac09615cebecd1936cd245db03bf3ac5601d4
junyoung-sim <eden920.dev@gmail.com>
2020-03-06 22:11:40 +0900

    Automated Commision

--- momentum/momentum.py
+++ momentum/momentum.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from service import download_stock, git_update
+from service import download_stock, git_update, graph
 from stock import upload
 
 def momentum_investing(stock, principal, evaluate_range, evaluate_period):
@@ -11,11 +11,15 @@ def momentum_investing(stock, principal, evaluate_range, evaluate_period):
     stock_asset = 0
     total_asset = principal
     previous_asset = 0
+    change_matrix = []
+    bh_change_matrix = []
     for _range in range(evaluate_range, len(stock) - evaluate_range, evaluate_period):
         momentum = 0
         total_asset = int(cash + (int(stock[_range]) * shares))
         change = ((total_asset - principal) / principal) * 100
         bh_change = (int(stock[_range]) - stock[evaluate_range]) * 100 / stock[evaluate_range]
+        change_matrix.append(change)
+        bh_change_matrix.append(bh_change)
         for i in range(_range, _range - evaluate_range, -1):
             if stock[_range] > stock[i]:
                 momentum += 1
@@ -27,6 +31,8 @@ def momentum_investing(stock, principal, evaluate_range, evaluate_period):
         print("Total = ", total_asset, " [ Stock = ", stock_asset, ", Cash = ", cash, "] --> Shares: ", shares, ", < CHANGE=", change, ">", "<BH Change = ", bh_change, ">", int(stock[_range]))
     print("Final Profit = ", (total_asset - principal) * 100 / principal)
     print(stock[0])
+    graph(change_matrix, 'red', 'momentum.jpg', False)
+    graph(bh_change_matrix, 'blue', 'momentum.jpg', False)
 
 if __name__ == "__main__":
     csv, id = download_stock()
