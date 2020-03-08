30117f047f087ef978ae3c854aad7c8aad9ddb3c
junyoung-sim <eden920.dev@gmail.com>
2020-03-08 11:35:27 +0900

    Automated Commision

--- prediction/sequential.py
+++ prediction/sequential.py
@@ -26,13 +26,13 @@ if __name__ == "__main__":
     for i in range(vix_timeseries.raw_size()):
         vix_average += vix_timeseries.raw_datapoint(i)
     vix_average /= vix_timeseries.raw_size()
-    print(vix_average)
 
     bias_momentum = 0.00 # smaller the better
     for i in range(timeseries.raw_size() - 1, timeseries.raw_size() - 10, -1):
         if timeseries.raw_datapoint(timeseries.raw_size() - 1) < timeseries.raw_datapoint(i):
             bias_momentum += 1
             bias_momentum *= (vix_average / 11)
+    print("\nBIAS = [", bias_momentum, "]\n")
 
     for count in range(5):
         trend = moving_average(timeseries, MONTH)
