045a89311d18aa3e695a3faa8b9f50698821a0ce
junyoung-sim <eden920.dev@gmail.com>
2020-03-13 18:31:05 +0900

    Automated Commision

--- prediction/sequential.py
+++ prediction/sequential.py
@@ -31,9 +31,8 @@ def sequential_prediction(model=None, stock_id=None, date=None, graphing=True, l
     for i in range(timeseries.raw_size() - 1, timeseries.raw_size() - 10, -1):
         if timeseries.raw_datapoint(i) < timeseries.raw_datapoint(i - 1):
             bias_momentum += 1
-            print(bias_momentum * votality_rate)
-            bias_momentum *= votality_rate
-    bias_momentum *= -1.00
+    bias_momentum *= -1.00 * votality_rate
+    print(bias_momentum)
 
     for count in range(5):
         trend = moving_average(timeseries, MONTH)
